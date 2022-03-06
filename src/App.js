2c1ff2c98f154283f72114b87fdc53cf9315b0f1
Meoweeen <stasefimen@mail.ru>
2022-03-06 15:31:11 +0300

    submit
    fucking refresh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from "react";
 
- function App() {
+function App() {
   const [toDoTitle, setToDoTitle] = useState();
   const [toDoArray, setToDoArray] = useState([]);
   const [toDoInfo, setToDoInfo] = useState();
@@ -9,68 +9,75 @@ import { useState } from "react";
   
   function handleChange(event){
     setToDoTitle(event.target.value);
-    };
+  };
 
   function handleChangeForInfo(event){
     setToDoInfo(event.target.value);
-    };
+  };
        
   function createToDo() {
-      const newToDoArray = [...toDoArray]
-      newToDoArray.push({
-        title: toDoTitle,
-        info: toDoInfo
-      });
-      setToDoArray(newToDoArray);
-      setToDoTitle("");
-      setToDoInfo("");
-    };
+    const newToDoArray = [...toDoArray]
+    newToDoArray.push({
+      title: toDoTitle,
+      info: toDoInfo
+    });
+    setToDoArray(newToDoArray);
+    setToDoTitle("");
+    setToDoInfo("");
+  };
 
-    
-    return (
-      <>
-        <div className="shadow">
-        <div className="core">
-        <div className="createToDo">
-          <input onChange={handleChange} value={toDoTitle} type="text" className="headerCreate" placeholder="Title"/> <br></br>
-          <textarea  onChange={handleChangeForInfo} value={toDoInfo} type="text" className="infoCreate" placeholder="Description"/> <br></br>
-          <button className="buttonCreate" onClick={createToDo}>  {"create daily quest"} </button>
-        </div>
+  return (
+    <div className="shadow">
+      <div className="core">
+        <form className="createToDo" onSubmit={createToDo}>
+
+          <input
+            onChange={handleChange}
+            value={toDoTitle}
+            type="text"
+            className="headerCreate"
+            placeholder="Title"
+            maxlength="10"
+            required
+          />
+
+          <textarea
+            onChange={handleChangeForInfo}
+            value={toDoInfo} type="text"
+            className="infoCreate"
+            placeholder="Description"
+            maxlength="10"
+            required
+          />
+
+          <button
+            className="buttonCreate"
+            type="Submit"
+          > 
+            {"create daily quest"} 
+          </button>
+
+        </form>
         
         <div className="toDoList" >
           {
             toDoArray.map(todo => {
               return (
-                <>
-                  <table cellSpacing="0" cellPadding="5" className="toDo">
-                   <tbody>
-                    <tr>
-                     <td  width="200"  ><p align="center" >{todo.title}</p><br></br><p className="toDoInfo">{todo.info}</p></td >
-                     <td  className="sec"><button className="btn" ></button></td >
-                    </tr>
-                   </tbody>
-                  </table>
-                </> )})
+               <table cellSpacing="0" cellPadding="5" className="toDo">
+                  <tbody>
+                  <tr>
+                    <td  width="200"  ><p align="center" >{todo.title}</p><br></br><p className="toDoInfo">{todo.info}</p></td >
+                    <td  className="sec"><button className="btn" ></button></td >
+                  </tr>
+                  </tbody>
+                </table>
+              )
+            })
           }
-          </div>
         </div>
-        </div>
-        
-      </>
-    );
- 
-    /*
-    function hide(){
-      let a=document.getElementById("ex").style;
-      if (a.display == "none") {
-        a.display="block"}else{
-          a.display="none"}
-          let exad = document.getElementsByClassName("info");
-    console.log(exad);
-    alert(exad.length);
-    };
-    */
-  }
-
+      </div>
+    </div>
+  );
+}
 
 export default App;
