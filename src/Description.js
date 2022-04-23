2b0e79e3f6ad16dda0fb1945d7b13b0deaa84607
Meoweeen <stasefimen@mail.ru>
2022-04-23 22:06:15 +0300

    snoop(бежит поёт)

--- src/Description.js
+++ src/Description.js
@@ -1,20 +1,20 @@
 import NavBar from "./NavBar";
-import Snoop from "./Snoop"
+import Snoop from "./Snoop";
+import React from 'react';
 
 export default function Description() {
   return (
-  <>
-    <NavBar/>
-    <Snoop/>
-    
-    <div className="crutch">
-      <div className="header">
-        <h2>Invoices</h2>
+    <>
+      <NavBar/>
+      <Snoop/>    
+      <div className="crutch">
+        <div className="header">
+          <h2>Invoices</h2>
+       </div>
+       <div className="info">
+         Description
+       </div>
       </div>
-      <div className="info">
-        Description
-      </div>
-    </div>
-  </>
+    </>
   );
 }
--- src/Snoop.js
+++ src/Snoop.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import useSound from 'use-sound';
 import Snoopgif from './img/snoop.gif';
+import Snoopmp3 from './img/snoop.mp3';
 
-export default function Snoop() {
-    return (
-       <>
-         <input type="checkbox" id="SnoopToggle" hidden ></input>
-         <div className="snoop">
-           <label for="SnoopToggle" className="SnoopToggle" onclick></label>
-           <img src={Snoopgif} height="200" width="75"></img>
-         </div>
-       </>
-     );  
-   }
+function Snoop() {
+  
+  const [play, { stop }] = useSound(      
+    Snoopmp3
+  );
+
+  const [isHovering, setIsHovering] = React.useState(
+    false
+  );
+
+  function playing() {
+   if(isHovering == false){
+      setIsHovering(true);
+      play();
+    }else{
+      setIsHovering(false);
+      stop();
+    }};
+  
+  return (
+    <>
+      <input type="checkbox" id="SnoopToggle" hidden ></input>
+      <div className="snoop">
+      <label for="SnoopToggle" className="SnoopToggle" onClick={playing}></label>
+        <img src={Snoopgif} height="200" width="75"></img>
+      </div>
+    </>
+  );  
+}
+
+export default Snoop;
