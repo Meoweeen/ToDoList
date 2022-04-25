174ac1e7295ccbc999abf7e3c8aeb7fda2f0b857
Meoweeen <stasefimen@mail.ru>
2022-04-25 07:45:26 +0300

    trying localStorage

--- src/App.js
+++ src/App.js
@@ -1,77 +1,16 @@
 import React from 'react'
-import { useState } from "react";
-import ToDo from "./ToDo";
+import ToDoForm from "./ToDo";
 import NavBar from "./NavBar"
 import Snoop from "./Snoop"
 
 function App() {
-  const [toDoTitle, setToDoTitle] = useState();
-  const [toDoArray, setToDoArray] = useState([]);
-  const [toDoInfo, setToDoInfo] = useState();
-  
-  function handleChange(event){
-    setToDoTitle(event.target.value);
-  };
-
-  function handleChangeForInfo(event){
-    setToDoInfo(event.target.value);
-  };
-
-  function createToDo(event) {
-    event.preventDefault();
-    const newToDoArray = [...toDoArray]
-    newToDoArray.push({
-      title: toDoTitle,
-      info: toDoInfo
-    });
-    setToDoArray(newToDoArray);
-    setToDoTitle("");
-    setToDoInfo("");
-  };
-
   return (
   <>
     <NavBar/>
     <Snoop/>
-
     <div className="shadow">
       <div className="core">
-        <form className="createToDo" onSubmit={createToDo}>
-
-          <input
-            onChange={handleChange}
-            value={toDoTitle}
-            type="text"
-            className="titleCreate"
-            placeholder="Title"
-            maxLength="20"
-            required
-          />
-
-          <textarea
-            onChange={handleChangeForInfo}
-            value={toDoInfo} 
-            type="text"
-            className="infoCreate"
-            placeholder="Description"
-            maxLength="1000"
-            required
-          />
-
-          <button
-            className="buttonCreate"
-            type="Submit"
-          > 
-            {"create daily quest"} 
-          </button>
-
-        </form>
-        
-        <div className="toDoList" >
-          {
-            toDoArray.map(todo =>  <ToDo elem={todo} />)
-          }
-        </div>
+        <ToDoForm/>
       </div>
     </div>
   </>
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from "react";
+import { useState , useEffect} from "react";
 
 function ToDo({elem}){
   const [toDoBoolean, setToDoBoolean] = useState(false);
@@ -29,4 +29,91 @@ function ToDo({elem}){
   )
 };
 
-export default ToDo;
+function ToDoForm(){
+
+ 
+
+  const [toDoTitle, setToDoTitle] = useState();
+  const [toDoInfo, setToDoInfo] = useState();
+  const [toDoArray, setToDoArray] = useState([]); // нихуя не одуплил нахуя тут скобки квадратные,ну типо при рендеринге первом нельзя что бы этот массив был пустой,ок,а с хуя ли нельзя?
+
+  useEffect(
+    () => {for (var i = 0; i < localStorage.length; i++) {
+    console.log(localStorage.getItem(localStorage.key(i)));
+    }
+});
+
+
+  
+
+  function handleChange(event){
+    setToDoTitle(event.target.value);
+  };
+
+  function handleChangeForInfo(event){
+    setToDoInfo(event.target.value);
+  };
+
+
+  function createToDo(event) {
+    event.preventDefault();
+    const newToDoArray = []
+    newToDoArray.push({
+      title: toDoTitle,
+      info: toDoInfo
+    });
+
+    
+
+    localStorage.setItem(toDoTitle, JSON.stringify(newToDoArray));
+    
+    setToDoTitle("");
+    setToDoInfo("");
+    setToDoArray(newToDoArray);
+  };
+
+
+
+  return(
+    <>
+      <form className="createToDo" onSubmit={createToDo}>
+
+        <input
+          onChange={handleChange}
+          value={toDoTitle}
+          type="text"
+          className="titleCreate"
+          placeholder="Title"
+          maxLength="20"
+          required
+        />
+
+        <textarea
+          onChange={handleChangeForInfo}
+          value={toDoInfo} 
+          type="text"
+          className="infoCreate"
+          placeholder="Description"
+          maxLength="1000"
+          required
+        />
+
+        <button
+          className="buttonCreate"
+          type="Submit"
+        > 
+          {"create daily quest"} 
+        </button>
+
+      </form>
+
+      <div className="toDoList" >
+        {
+         toDoArray.map(todo =>  <ToDo elem={todo} />)
+        }
+      </div>
+    </>
+  )
+};
+
+export default ToDoForm;
